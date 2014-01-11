c9f24b5e146d30fd664a0adf4c051d2ff75ef91a
Stacy Faude <istacy@fullsail.edu>
2014-01-11 08:32:18 -0600

    Start of Project - Added Variables

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,28 @@
+/* Created by stacy on 1/8/14.
+* Name: Stacy Faude
+* Date: 1-8-14
+* Assignment: Goal 1 - Assignment
+* */
+
+
+//self-executing function
+(function (){
+
+    console.log("FIGHT!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //rounds
+    var round = 1;
+
+})();
