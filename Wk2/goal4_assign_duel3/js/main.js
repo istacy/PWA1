88ffba47b51ce18fdeec7b7ab0cc7274499a61b8
Stacy Faude <istacy@fullsail.edu>
2014-01-18 15:59:33 -0600

    Week 2 - Goal 4 Duel 3

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -20,36 +20,43 @@
     function fight(){
         //console.log("in the fight function");
 
-        alert(player1[0]+":"+player1[2]+" *START* "+player2[0]+":"+player2[2]);
+//        alert(player1[0]+":"+player1[2]+" *START* "+player2[0]+":"+player2[2]);
 
         for (var i = 0; i < 10; i++){
 
             //random formula is - Math.floor(Math.random() * (max - min) + min);
 
-            var minDamage1 = player1[1] * .5;
-            var minDamage2 = player2[1] * .5;
-            var f1 = Math.floor(Math.random()*(player1[1]-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(player2[1]-minDamage2)+minDamage2);
+            //var minDamage1 = player1[1] * .5;
+            //var minDamage2 = player2[1] * .5;
+            //var f1 = Math.floor(Math.random()*(player1[1]-minDamage1)+minDamage1);
+            //var f2 = Math.floor(Math.random()*(player2[1]-minDamage2)+minDamage2);
+            var minDamage1 = player1.damage * .5;
+            var minDamage2 = player2.damage * .5;
+            var f1 = Math.floor(Math.random()*(player1.damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2.damage-minDamage2)+minDamage2);
 
             //console.log(f1);
             //console.log(f2);
 
             //inflict damage
-            player1[2]-=f1;
-            player2[2]-=f2;
+            //player1[2]-=f1;
+            //player2[2]-=f2;
+            player1.health-=f1;
+            player2.health-=f2;
 
             //console.log(playerOneHealth);
             //console.log(playerTwoHealth);
 
-            console.log(player1[0]+": "+player1[2] + "  " + player2[0]+":"+player2[2]);
+            //console.log(player1[0]+": "+player1[2] + "  " + player2[0]+":"+player2[2]);
+            console.log(player1.name+": "+player1.health + "  " + player2.name+":"+player2.health);
 
             var result = winnerCheck();
             console.log(result);
             if (result==="no winner"){
                 round++;
-                alert(player1[0]+":"+player1[2]+"  *ROUND "+round+" OVER"+"*  "+player2[0]+":"+player2[2]);
+//                alert(player1[0]+":"+player1[2]+"  *ROUND "+round+" OVER"+"*  "+player2[0]+":"+player2[2]);
             }else{
-                alert(result);
+//                alert(result);
                 break;
             };
 
