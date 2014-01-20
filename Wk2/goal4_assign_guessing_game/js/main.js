8aa472734464384e1a6e85306aa47bac57a22e9b
Stacy Faude <istacy@fullsail.edu>
2014-01-20 11:47:54 -0600

    Week 2 - Goal 4 guessing game
    Starting Over!!!

--- Wk2/goal4_assign_guessing_game/js/main.js
+++ Wk2/goal4_assign_guessing_game/js/main.js
@@ -8,6 +8,22 @@
  */
 
 //Game variables
-(function(){
+(function (){
+    var x = Math.floor(Math.random() * 10) + 1;
+    var turns = 3;
+    var dom = {
+        input: document.querySelector("#input"),
+        output: document.querySelector("#output"),
+        button: document.querySelector("button")
+    };
+
+
+    var validateInput = function(){
+        playersGuess = parseInt(dom.input.value);
+
+        if(isNAN(playersGuess)){//check for proper data type
+
+        }
+    };
 
 })();
