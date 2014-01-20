7052d3b5f31e59663e9795823510b29d3065f0cc
Stacy Faude <istacy@fullsail.edu>
2014-01-20 11:08:09 -0600

    Week 2 - Goal 4 Guessing Game
    I have no idea what I am doing…

--- Wk2/goal4_assign_guessing_game/js/main.js
+++ Wk2/goal4_assign_guessing_game/js/main.js
@@ -32,32 +32,32 @@
 //alert('The secret number was ' + x + '.');
 //
 //})();
-
-(function (){
-    var x = Math.floor(Math.random() * 10) + 1;
-    var turns = 3;
-    var dom = {
-        input: document.querySelector("#input"),
-        output: document.querySelector("#output"),
-        button: document.querySelector("button")
-    };
-
-    var status;
-
-    while (turns > 0){
-        var guess = (document.querySelector("output"));
-        if (!guess) break;
-        guess = Number(guess);
-        if (guess == x) {
-            document.querySelector("output").innerHTML("You win");
-            turns = 0;
-        } else {
-            document.querySelector("output").innerHTML("Guess Again");
-            if (guess < x) document.querySelector("output").innerHTML("Too Small!");
-            if (guess > x) document.querySelector("output").innerHTML("Too Big!");
-            turns = turns - 1;
-        }
-    }
+//
+//(function (){
+//    var x = Math.floor(Math.random() * 10) + 1;
+//    var turns = 3;
+//    var dom = {
+//        input: document.querySelector("#input"),
+//        output: document.querySelector("#output"),
+//        button: document.querySelector("button")
+//    };
+//
+//    var status;
+//
+//    while (turns > 0){
+//        var guess = (document.querySelector("output"));
+//        if (!guess) break;
+//        guess = Number(guess);
+//        if (guess == x) {
+//            document.querySelector("output").innerHTML("You win");
+//            turns = 0;
+//        } else {
+//            document.querySelector("output").innerHTML("Guess Again");
+//            if (guess < x) document.querySelector("output").innerHTML("Too Small!");
+//            if (guess > x) document.querySelector("output").innerHTML("Too Big!");
+//            turns = turns - 1;
+//        }
+//    }
 //
 //    var validateInput = function(){
 //        playersGuess = parseInt(dom.input.value);
@@ -66,5 +66,47 @@
 //
 //        }
 //    };
+//
+//})();
 
-})();
+
+
+//var x = Math.floor(Math.random() * 10) + 1;
+//var turns = 0;
+//var maxTurns = 3;
+//var dom = {
+//    input: document.querySelector("#input"),
+//    output: document.querySelector("#output"),
+//    button: document.querySelector("button")
+//    };
+//
+//function amIRight(guess){
+//    if (guess == ""){
+//        dom.output("You did not make a guess");
+//        return;
+//    }
+//    if (isNaN(guess)){
+//        dom.output("You must enter a valid number");
+//        return;
+//    }
+//    if (guess == randomNumber){
+//        dom.output("Great guess\nYou had " + numberOfGuesses + " tries");
+//        return;
+//    }
+//    if (guess > randomNumber){
+//        dom.output("Too high");
+//        numberOfGuesses++;
+//        return;
+//    }
+//    if (guess < randomNumber){
+//        dom.output("Too Low");
+//        numberOfGuesses++;
+//        return;
+//    }
+//}
+//function newGame(){
+//    randomNumber = Math.round(Math.random() * 10) + 1;
+//    numberOfGuesses = 0;
+//    dom.input.value="";
+//}
+
