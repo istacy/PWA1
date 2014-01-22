cc15f6f7fc1f40907f9257a370dffadd388e53d7
Stacy Faude <istacy@fullsail.edu>
2014-01-22 11:16:07 -0600

    Week 2 - Goal 5 Validation

--- Wk2/goal5_assign_validation/js/validate.js
+++ Wk2/goal5_assign_validation/js/validate.js
@@ -7,6 +7,12 @@
      Goal 5 Assignment - Validation
 */
 
+// username (id = f_username) --> ^([A-Z]+[a-zA-Z]*)(\s|\-)?([A-Z]+[a-zA-Z]*)?(\s|\-)?([A-Z]+[a-zA-Z]*)?$
+// email (id = f_email) --> /^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]+$/
+// phone number (id = f_phone) -->  ^\(?\d{3}?\)?\-?\d{3}?\-?\d{4}?$
+// social security number (id = f_ssn) -->  ^\d{3}-\d{2}-\d{4}$
+// password (id = f_password) -->  [a-zA-Z]\w{3,14}$
+
 (function(){
 
     myform.onsubmit = function(e){
@@ -24,16 +30,16 @@
 
     var validateField = function(inputName){
 
-        if (inputName.name === 'id name goes here'){
-            var pattern = 'a RegEx pattern goes here'
+        if (inputName.name === 'f_username'){
+            var pattern = '^([A-Z]+[a-zA-Z]*)(\s|\-)?([A-Z]+[a-zA-Z]*)?(\s|\-)?([A-Z]+[a-zA-Z]*)?$'
 
             //You will need to create an else-if statement for each input field id.  The
             //      format will be similar to the above IF statement.
 
 
-        };
+        }
 
-        var pass = 'the RegEx .test statement is needed here';
+        var pass = '^[a-zA-Z]\w{3,14}$';
         var errorMsg = inputName.nextSibling.nextSibling.nextSibling.nextSibling;
 
         if (!pass || inputName.value.length < 2){
@@ -45,7 +51,7 @@
         } else {
             errorMsg.style.display='none';
             inputName.style.backgroundColor = 'white';
-        };
+        }
     };
 
 })();  // end wrapper
