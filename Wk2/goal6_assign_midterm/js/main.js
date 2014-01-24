d20151d12b883bed7282f7e5915c4ac3d5540520
Stacy Faude <istacy@fullsail.edu>
2014-01-24 09:45:45 -0600

    Week 2 - Goal 6 Midterm

--- Wk2/goal6_assign_midterm/js/main.js
+++ Wk2/goal6_assign_midterm/js/main.js
@@ -1,3 +1,17 @@
 /*
+
+Stacy Faude
+1-23-14
 	* Mid Terms for PWA-1
 */
+
+
+//Global Variable #1: the current student record to display
+var record = 0;
+
+//Global Variable #2: Students
+var students = [
+    {name: 'Stacy Faude', address:{address: '2000 Snowcovered Road', city: Chicago, state: Illinois}, gpa: [4.0, 4.0, 4.0]},
+    {name: 'Lady Gaga', address:{address: '1234 Rockstar Road', city: Los Angeles, state: California}, gpa: [3.0, 2.5, 3.8]},
+    {name: 'Luke Bryan', address:{address: '9876 Country Road', city: Smalltown, state: Georgia}, gpa: [3.8, 4.0, 3.6]}
+    ];
